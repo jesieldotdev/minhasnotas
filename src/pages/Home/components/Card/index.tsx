8dcfc1372bdd114f0e9402dab7e60186cefdb6a1
jesieldotdev <jesiel364@gmail.com>
2024-06-08 14:13:57 -0400

    feat: done state

--- src/pages/Home/components/Card/index.tsx
+++ src/pages/Home/components/Card/index.tsx
@@ -1,17 +1,27 @@
 import { Check } from "lucide-react"
+import { useState } from "react"
 
 interface CardProps {
-
+    isChecked: boolean
 }
 
-export const Card = () => {
+export const Card = ({ isChecked }: CardProps) => {
+    const [done, setDone] = useState(false)
+
+    function onChangeState() {
+        setDone(!done)
+        return
+    }
     return (
-        <div className="flex justify-between border-slate-300 border-2 rounded-md p-4">
+        <div onClick={() => onChangeState()} className="flex justify-between border-slate-300 border-2 rounded-md p-4">
             <div>
                 <p className="font-medium ">Organize seu app baba bababa</p>
                 <p className="mt-6 text-sm text-slate-500">15 janeiro 2024 10:00 AM</p>
             </div>
-            <Check />
+            {
+                done ? <Check /> : null
+            }
+
 
         </div>
     )
