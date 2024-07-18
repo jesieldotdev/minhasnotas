d5688419d137375cd5bd8009776286df0cdbf73f
jesieldotdev <jesiel364@gmail.com>
2024-07-17 23:54:54 -0400

    login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -46,22 +46,26 @@ const Login: React.FC = () => {
 
             <Header />
 
+            <p className='mt-[-16px] mb-8 '>Seu organizador pessoal de tarefas.</p>
 
-                <h2 className="text-2xl font-bold mb-6">Entrar</h2>
+
+                <h2 className="text-2xl font-bold mb-6">Faça seu acesso</h2>
                 <div className="mb-4">
-                    <label className="block text-gray-700 mb-2">Email:</label>
+                    <label className="block text-gray-700 mb-2">Email</label>
                     <input
                         type="email"
+                        placeholder='johndoe@email.com'
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
                         className="w-full px-3 py-2 border rounded"
                     />
                 </div>
                 <div className="mb-4">
-                    <label className="block text-gray-700 mb-2">Senha:</label>
+                    <label className="block text-gray-700 mb-2">Senha</label>
                     <input
                         type="password"
                         value={password}
+                        placeholder='sua senha aqui'
                         onChange={(e) => setPassword(e.target.value)}
                         className="w-full px-3 py-2 border rounded"
                     />
